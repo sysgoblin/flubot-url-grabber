b4f8f3968020fd97cb45b51b47bd5f6127f9ff0d
sysgoblin <cbaylissmk2@gmail.com>
2021-04-24 11:03:35 +0100

    add input arg, batch async requests to speed up collection

--- README.md
+++ README.md
@@ -6,22 +6,63 @@
 
 This script will continually hit the provided flubot lure URL to enumerate and collect unique download URLs or domains, based on the provided arguments.
 
+## Setup
+**Step 1**: Clone the repo
+```
+git clone https://github.com/sysgoblin/flubot-url-grabber.git
+```
+**Step 2**: cd in to the repo folder
+```
+cd flubot-url-grabber/
+```
+**Step 3**: Install the requirements (python 3.6+)
+```
+pip3 install requirements.txt
+```
+
+## Usage
+Required arguments:
+```
+-u --url        URL to collect from
+
+OR
+
+-i --input      List of newline delimited URLs to collect from
+```
+
+Optional arguments:
+```
+-p --path       Path of output file to save collected URLs/domains
+
+--domain-only   Return domains only, not full URLs
+```
+
 ## Examples
-```bash
-$ python3 grabber.py -u http://exmaple.com/pkg/?2gfv5gfdbavg
+Output unique full URLs collected from the provided URL.
+```
+$ python3 grabber.py --url http://exmaple.com/pkg/?2gfv5gfdbavg
 
 https://www.domain1.com/xyz/?zwd3v4vu32v4rivvt3fz75eyysb7baxxut677n74wkl5hhnxrlki...
 https://kf.domain2.com/xyz/?el4fldk3ufclotokog2hbc3xxnwia34ijobbbsatzv4ky4wimwvw3...
 https://domain3.ir/xyz/?fiwv2wctorggeri7pfqxqxt7nzsfkzyyinlrqhi3dbyhs6q5nv7gkfdbp...
 ```
-Output unique full URLs collected from the provided URL.
 
-```bash
+Output unique domains collected from the provided URL, and also save to the file `out.txt`
+```
 $ python3 grabber.py -u http://exmaple.com/pkg/?2gfv5gfdbavg -p ./out.txt --domain-only
 
 https://domain1.net
 https://www.domain2.com
 https://domain3.com.vn
 ```
-Output unique domains collected from the provided URL, and also save to the file `out.txt`
+
+Iterate over provided URLs within input.txt (newline delimited) and print all unique domains collected
+```
+$ python3 grabber.py -i input.txt --domain-only
+
+https://domain1.net
+https://www.domain2.com
+https://domain3.com.vn
+```
+
 
--- grabber.py
+++ grabber.py
@@ -1,26 +1,14 @@
+#!/usr/bin/python3
 import argparse
 import os
 import re
 import signal
 import sys
 from urllib.parse import urlparse
+from concurrent.futures import as_completed
+from requests_futures.sessions import FuturesSession
 
 import requests
-
-
-class Tee(object):
-    def __init__(self, *files):
-        self.files = files
-
-    def write(self, obj):
-        for f in self.files:
-            f.write(obj)
-            f.flush()
-
-    def flush(self):
-        for f in self.files:
-            f.flush()
-
 
 def handler(signum, frame):
     exit(0)
@@ -37,18 +25,27 @@ def is_valid_file(arg):
 
 def setup_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-u', '--url', help='URL of flubot downloader', required=True)
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-u', '--url', help='URL of flubot downloader')
+    group.add_argument('-i', '--input', help='Path for input file containing URLs to scan', type=is_valid_file)
     parser.add_argument('-p', '--path', help='Path for file output', required=False, type=is_valid_file)
     parser.add_argument('--domain-only', dest="domain", help='Return domains only, not full URLs', required=False, action="store_true", default=False)
     return parser
 
 
 regex = re.compile(r'(?:class="btn" href=")([^"]*)(?:">)')
-session = requests.session()
+# session = requests.session()
+session = FuturesSession()
 session.headers = {
     'User-Agent': 'Mozilla/5.0 (Linux; Android 8.0.0; PRA-LX3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Mobile Safari/537.36'
 }
 grabbed = []
+
+def output(match, path):
+    print(match)
+    if path is not None:
+        with open(path, 'a+') as f:
+            f.write(match + "\n")
 
 
 if __name__ == '__main__':
@@ -56,35 +53,42 @@ if __name__ == '__main__':
     p = setup_args()
     args = p.parse_args()
 
-    url = args.url
+    if args.input:
+        with open(args.input, 'r') as f:
+            urls = f.read().splitlines()
+    elif args.url:
+        urls = [args.url]
+    else:
+        print("No input?")
+        exit()
+
     domain = args.domain
 
-    if args.path:
-        f = open(args.path, "a+")
-        sys.stdout = Tee(sys.stdout, f)
+    for url in urls:
+        loop = True
+        while loop:
+            new_count = 0
+            futures = [session.get(url) for i in range(10)]
+            for future in as_completed(futures):
+                req = future.result()
+                if not req.ok:
+                    print("Site looks down!")
+                    continue
 
-    no_new_count = 0
-    while no_new_count < 20:
-        req = session.get(url)
-        if not req.ok:
-            print("Site looks down!")
-            exit()
+                match = regex.findall(req.text)[0]
 
-        matches = regex.findall(req.text)
+                if not match:
+                    print("No download URLs detected")
+                    continue
 
-        if len(matches) == 0:
-            print("No download URLs detected")
-            exit()
-
-        for match in matches:
-            if domain:
-                up = urlparse(match)
-                match = up.scheme + '://' + up.hostname
-            if match not in grabbed:
-                grabbed.append(match)
-                no_new_count = 0
-                print(match)
-            else:
-                no_new_count += 1
-
-    print("No new URLs detected for 20 attempts. Stopping!")
+                if domain:
+                    up = urlparse(match)
+                    match = up.scheme + '://' + up.hostname
+                if match not in grabbed:
+                    grabbed.append(match)
+                    new_count += 1
+                    output(match, args.path)
+                
+                if new_count == 0:
+                    loop = False
+        print("No new URLs detected for 10 attempts. Stopping!")
